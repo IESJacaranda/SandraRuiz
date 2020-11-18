d8cdca6436320c310ac133486b3463cf1b2fbc82
sandra <sandra.ruizjimenez86@gmail.com>
2020-11-18 09:22:46 +0100

    Creacion ejercicio 4 18/11 clase

---
+++ prog1/Bucles3/Ejercicio4.py
@@ -0,0 +1,22 @@
+"""
+Created on 17 nov.2020
+@author: Sandra Ruiz Jimenez
+
+Ejercicio 4
+
+Diseñar un programa que muestre un menú con las siguientes opciones:
+
+1. Cambio de euros a dólares
+2. Cambio de dólares a euros
+3. Cambio de euros a libras
+4. Cambio de libras a euros.
+5. Cambio de libras a dólares
+6. Cambio de dólares a libras
+7. Salir
+
+El programa debe pedir una opción, luego una cantidad según corresponda y 
+mostrar la conversión correspondientes. 
+El programa terminará cuando pulse un 7.
+
+"""
+
