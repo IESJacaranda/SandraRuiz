67d125df94eeff71c69a7b19fe8d12b1a5cf6b20
sandra <sandra.ruizjimenez86@gmail.com>
2020-11-19 17:07:19 +0100

    Modificados y ejercicio 5 hecho 19/11 casa

--- prog1/Bucles3/Ejercicio5.py
+++ prog1/Bucles3/Ejercicio5.py
@@ -7,3 +7,27 @@ Realizar un programa que solicite la coordenada x e y de un punto e informe si s
 en el primer, segundo, tercer o cuarto cuadrante.
 
 """
+
+def calculaCoordenada ():
+    x=1
+    while x!=0:
+        x=int(input("Dime la coordenada x: "))
+        y=int(input("Dime la coordenada y: "))
+        if x>0:
+            if y>0:
+                print("El punto se encuentra en el segundo cuadrante.")
+            elif y<0:
+                print("El punto se encuentra en el cuarto cuadrante.")
+            else:
+                print("Error, el punto no se encuentra en ningún cuadrante.")
+        elif x<0:
+            if y>0:
+                print("El punto se encuentra en el primer cuadrante.")
+            elif y<0:
+                print("El punto se encuentra en el tercer cuadrante.")
+            else:
+                print("Error, el punto no se encuentra en ningún cuadrante.")
+        else:
+            print("Error, el punto no se encuentra en ningún cuadrante.")
+        
+calculaCoordenada()
