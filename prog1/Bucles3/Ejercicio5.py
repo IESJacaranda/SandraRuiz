715129fca3333f3371a4494ee0bab13e15b71083
sandra <sandra.ruizjimenez86@gmail.com>
2020-11-19 16:53:01 +0100

    Añadidos ejercicios 5, 6, 7 y 8 19/11 casa

---
+++ prog1/Bucles3/Ejercicio5.py
@@ -0,0 +1,9 @@
+"""
+Created on 17 nov.2020
+@author: Sandra Ruiz Jimenez
+
+Ejercicio 5
+Realizar un programa que solicite la coordenada x e y de un punto e informe si se encuentre
+en el primer, segundo, tercer o cuarto cuadrante.
+
+"""
