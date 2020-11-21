545ea62227b3178aa87ffc3d9c01d5005cebb425
sandra <sandra.ruizjimenez86@gmail.com>
2020-11-21 18:34:19 +0100

    Añadidos ejercicio 1, 2 y 3

---
+++ prog1/Bucles1/Ejercicio1.py
@@ -0,0 +1,21 @@
+"""
+Ejercicio 1
+Diseñar un programa para aprender a contar. El programa pedirá un número y mostrará todos los números comprendidos entre el 1 y el número introducido.
+"""
+#Solución usando bucle for
+
+
+numero = int(input("¿Hasta qué número quieres contar?"))
+for i in range(1,numero+1):
+    print(i)
+    
+"""
+Solución usando bucle while:
+
+numero = int(input("¿Hasta qué número quieres contar?"))
+i = 1
+while i <= numero:
+    print(i)
+    i = i + 1
+
+"""
---
+++ prog1/Bucles1/Ejercicio4.py
@@ -0,0 +1,7 @@
+"""
+
+Solución con bucle for:
+"""
+numero = int(input("¿Hasta qué número quieres contar?"))
+for i in range(1,numero+1):
+    print(i)
