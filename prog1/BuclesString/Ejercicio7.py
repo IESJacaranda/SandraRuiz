260b2ef8cfea98d524f8565b50bf78282fc802d5
sandra <sandra.ruizjimenez86@gmail.com>
2020-11-30 20:21:53 +0100

    Ejercicio 7

--- prog1/BuclesString/Ejercicio7.py
+++ prog1/BuclesString/Ejercicio7.py
@@ -10,29 +10,13 @@ def buscarYReemplazar(frase, palabra, reemplazo):
         return frase
     fraseMod = ""
     iFrase = 0
+    iPalabra = 0
+    iReemplazo= 0
     
-    for i in range (0, len(frase):
-        if frase[i]==palabra[i]:
-            if palabra[i] == reemplazo[i]:
-                iReemplazo+=fraseMod.replace(palabra[i], reemplazo[i])
-        iFrase+=1
-        print(fraseModificada)
-    return fraseModificada
-    
+    while iPalabra<len(palabra) and frase[iFrase]==palabra[iPalabra]:
+        fraseMod=frase.replace(palabra, reemplazo)
+    return fraseMod    
 
-"""    
-    while para recorrer la fraseEjemplo.
-    En cuanto tenga un match entre primer elemento palabra y primer elemento de frase, voy a comprobar los demas elementos.
-    si coinciden utilizo la sustitucion. Para eso uso variable auxiliar que es el mensaje de salida.
-    Si veo que no coincide 
-        
-        utilizaría mas de while
-        print()
-
-            
-           
-    return "" 
-"""
 
 frase = "La lluvia en Sevilla es una maravilla, porque en Sevilla hace mucho calor"
 
