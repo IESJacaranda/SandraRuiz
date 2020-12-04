9271cbd41902d0d8b9866f6e570da37a45f132ca
sandra <sandra.ruizjimenez86@gmail.com>
2020-12-04 20:25:36 +0100

    Modificado ej 3 examen

--- prog1/Examen2/Ejercicio3.py
+++ prog1/Examen2/Ejercicio3.py
@@ -2,24 +2,30 @@
 Examen 2 Diciembre - Ejercicio 3
 
 Realizar un programa que nos sirva para crear el id de usuario que nuestros empleados 
-utilizarán para conectarse a nuestras aplicaciones. El programa debe preguntarle al 
-usuario su nombre y sus apellidos, separando los datos por coma. Es decir si me 
-preguntan a mi yo debo poner JoséManuel,Sevillano,Armenta (puedes suponer que nos 
-van a dar los valores bien). Luego debe preguntar el dni, debe comprobar que se 
-trata de un dni válido, si no es válido volver a preguntarlo hasta que nos dé un 
-dni válido. Por último debe llamar a una función que devuelva el id. Para calcular 
-el id le pasaremos la cadena con el nombre y apellidos y creará el id con los tres 
+utilizarán para conectarse a nuestras aplicaciones. 
+El programa debe preguntarle al usuario su nombre y sus apellidos, separando los datos por coma. 
+Es decir si me preguntan a mi yo debo poner JoséManuel,Sevillano,Armenta (puedes suponer que nos 
+van a dar los valores bien). 
+Luego debe preguntar el dni, debe comprobar que se trata de un dni válido, si no es válido volver 
+a preguntarlo hasta que nos dé un dni válido. 
+Por último debe llamar a una función que devuelva el id. 
+Para calcular el id le pasaremos la cadena con el nombre y apellidos y creará el id con los tres 
 primeros caracteres del nombre, los tres últimos del primer apellido , los tres 
 primeros del segundo apellido y los tres primeros números del dni.
 """
 
 def nombreApellidos(cadena):
+    nombreApellidos=nombreApellidos.lower()
+    
+    for i in range (0,len(nombreApellidos)):
+        if nombreApellidos[i]==",":
+            indicadorNombre==
     return 
 
 
 
-nombre=int(input("Dime tu nombre: "))
-apellidos=int(input("Dime tus apellidos"))
+nombreApellidos=int(input("Introduce tu nombre y apellidos separados por coma: "))
+
 
 def calcularLetra(numeroDNI):
     return letras[numeroDNI % 23]
