92b7fd34b6d6c9d48e7b8c4ba6d4bee7d8d06fed
sandra <sandra.ruizjimenez86@gmail.com>
2020-12-09 09:30:54 +0100

    Resuelto ejercicio 9/12

--- prog1/Examen2/Ejercicio3.py
+++ prog1/Examen2/Ejercicio3.py
@@ -12,25 +12,18 @@ Por último debe llamar a una función que devuelva el id.
 Para calcular el id le pasaremos la cadena con el nombre y apellidos y creará el id con los tres 
 primeros caracteres del nombre, los tres últimos del primer apellido, los tres 
 primeros del segundo apellido y los tres primeros números del dni.
+
+Pasos del algoritmo:
+
+1. Solicitar datos
+    a. Nombre y Apellidos
+    b. DNI ==> validar
+2. Separamos nombre y apellidos ==> lista
+3. Llamamos a la función que nos genera el id de usuario
 """
 
-def nombreApellidos(nombre):
-    nombre=nombre.lower()
-    id=""
-    for i in range (0, 3):
-        id+=nombre[i]
-    
-
-
-
-
-nombre=input("Introduce tu nombre y apellidos separados por coma: ")
-
-nombreApellidos(nombre)
-
-"""
 def calcularLetra(numeroDNI):
-    return letras[numeroDNI % 23]
+    return "TRWAGMYFPDXBNJZSQYHLCKE"[numeroDNI % 23]
 
 
 def separarNumeroDNI(dniConLetra):
@@ -48,8 +41,87 @@ def esDniValido(dniConLetra):
     
     return resultado
 
-letras= "TRWAGMYFPDXBNJZSQYHLCKE"    
-dniValido7Cifras = "7777777M"
-dniValido8Cifras = "14141414W"
+
 
 """
+Esta función recibe una cadena de texto con datos identificativos (nombre y apellidos) 
+sin espacios entre ellos y separados por comas y devuelve una lista con los anteriores de
+forma individual
+"""
+def separarCadena(nombre):
+    lista= []
+    tmp = ""
+    for i in nombre:
+        if i != ",":
+            tmp+=i
+        else:
+            lista.append(tmp)
+            tmp=""
+    lista.append(tmp) #para que añada a la lista el segundo apellido
+    
+    return lista
+
+
+
+#assert(separarCadena("JoseManuel,Sevillano,Armenta")==["JoseManuel","Sevillano","Armenta"])
+#assert(separarCadena("Sandra,Ruiz,Jimenez")==["Sandra", "Ruiz","Jimenez"])
+
+
+
+"""
+A partir de una lista con nombre y paellidos junto al dni construye un identificador de
+usuario de como máximo 12 caracteres
+"""
+def generaIdUsuario(listaIdentificadores, dni):
+    idUsuario=""
+    listaIdentificadores.append(dni) #de esta manera me quedo con una sola lista
+    
+    for i in range (0,len(listaIdentificadores)): #la i toma 4 valores, los 4 elementos de la lista
+        if i!=1: 
+            for j in range(3): #nombre,apellido2 y dni, necesito las 3 primeras posiciones
+                idUsuario += listaIdentificadores[i][j]
+        else:
+            for k in range(-3,0): #apellido1, necesito las 3 ultimas posiciones
+                idUsuario += listaIdentificadores[i][k]
+                        
+    return idUsuario.lower()
+
+
+"""
+Otra solución con variables y sin bucle:
+
+def generaIdUsuario(listaIdentificadores, dni):
+    idUsuario=""
+    listaIdentificadores.append(dni)
+        
+    nombre = listaIdentificadores[0]
+    apellido1 = listaIdentificadores[1]
+    apellido2 = listaIdentificadores[2]
+    
+    idUsuario = nombre[0] + nombre[1] + nombre[2]
+    idUsuario += apellido1[-3] + apellido1[-2] + apellido1[-1]
+    idUsuario += apellido2[0] + apellido2[1] + apellido2[2]  
+    idUsuario += dni[0] + dni[1] + dni[2]  
+    
+    return idUsuario.lower()
+
+"""
+
+
+nombreSeparado=["Miguel","DeCervantes","Saavedra"]
+dniValido8Cifras = "14141414W"
+
+assert(generaIdUsuario(nombreSeparado, dniValido8Cifras)=="migtessaa141")
+
+
+
+def funcionPrincipal():
+    nombre_apellidos=input("Introduce tu nombre y apellidos separados por coma: ")
+    dni=input("Introduce tu dni en formato 11111111X: ")
+    
+    while (not esDniValido(dni)):
+        dni=input("Error. Introduce tu dni en formato 11111111X: ") 
+        
+    print("El id de usuario es %s" % generaIdUsuario(separarCadena(nombre_apellidos), dni))
+    
+funcionPrincipal()
