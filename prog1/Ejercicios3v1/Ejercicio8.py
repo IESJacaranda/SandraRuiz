18b418b17387b5cff590b39de544d66d3d249833
sandra <sandra.ruizjimenez86@gmail.com>
2020-12-15 14:22:41 +0100

    Corregido

--- prog1/Ejercicios3v1/Ejercicio8.py
+++ prog1/Ejercicios3v1/Ejercicio8.py
@@ -14,8 +14,8 @@ def secondOrder(a,b,c):
     radical = b**2 - 4*a*c
     
     if radical >=0 and a>0: #de esta manera evitamos las soluciones con numeros complejos
-        x1 = -b + math.sqrt(radical) / 2*a
-        x1 = -b - math.sqrt(radical) / 2*a
+        sol1 = (-b + math.sqrt(radical)) / 2*a
+        sol2 = (-b - math.sqrt(radical)) / 2*a
         
         if radical==0:
             soluciones.append(sol1)
@@ -27,9 +27,9 @@ def secondOrder(a,b,c):
     return soluciones
 
 #casos de prueba no sirven
-assert(secondOrder(-1,7,-10)==[2,5])
-assert(secondOrder(0,7,-10)==[-1])
-assert(secondOrder(1,-2,1)==1/2, 1/3)
+assert(secondOrder(1,-5,6)==[3.0,2.0])
+assert(secondOrder(1,-2,1)==[1])
+assert(secondOrder(1,1,1)==None)
 
 
 '''
@@ -40,11 +40,13 @@ def secondOrder(a,b,c):
     
     radical = b**2 - 4*a*c
     
-    if radical==0:
-        soluciones.append(-b + math.sqrt(radical) / 2*a)
-    else:
-        soluciones.append(-b + math.sqrt(radical) / 2*a)
-        soluciones.append(-b - math.sqrt(radical) / 2*a)
+    if radical==0 and a>0:
+        soluciones.append((-b + raiz) / 2*a)
+    elif radical>=0 and a>0:
+        raiz = math sqrt(radical)
+        soluciones.append((-b + raiz) / 2*a)
+        soluciones.append((-b - raiz) / 2*a)
 
             
     return soluciones
+'''
