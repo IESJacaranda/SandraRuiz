bf28fe3dab8abb3bfc4915298be3dd822c7eb24b
sandra <sandra.ruizjimenez86@gmail.com>
2020-12-16 09:22:28 +0100

    Corregidos

--- prog1/Ejercicios3v1/Ejercicio10.py
+++ prog1/Ejercicios3v1/Ejercicio10.py
@@ -11,7 +11,7 @@ Ejercicio 10
 
 def sumDivisor(number):
     sum=0
-    for i in range(1,number-1):
+    for i in range(1,number//+1):
         if number%i==0:
             sum+=i
     return sum    
@@ -30,3 +30,52 @@ assert(friend(1184, 1210)==True)
 assert(friend(0, 120)==False)
 assert(friend(748,-1)==False)
 assert(friend(10,20)==False)
+
+'''
+Solución clase:
+
+def getDivisors(number):
+    divisors = []
+       
+    if number > 0:
+        for i in range (1,(number//2)+1):
+            if number%i==0:
+                divisors.append(i)
+    
+    return divisors
+
+def sumaListaNumeros(lista):
+    sumaTotal=0
+    
+    for elemento in lista:
+        sumaTotal+=elemento
+        
+    return sumaTotal
+            
+assert(sumaListaNumeros([])==0)
+assert(sumaListaNumeros([1,3,5,9])==18)      
+   
+
+def areFriends(number1, number2):
+    friends = False
+    if number1!=number2 and number1>0 and number2>0:
+        sumaDiv_1=sumaListaNumeros(getDivisors(number1))
+        sumaDiv_2=sumaListaNumeros(getDivisors(number2))
+        friend = (number1==sumaDiv_2) and (number2==sumaDiv_1) 
+    
+    return friends
+
+'''
+'''
+* más reducido:
+
+def areFriends(number1, number2):
+    return number1!=number2 and number1==sumaListaNumeros(getDivisors(number2)) and number2==sumaListaNumeros(getDivisors(number1))_
+
+assert(areFriends(220, 284))
+assert(areFriends(1184, 1210))
+assert(not areFriends(0, 120))
+assert(not areFriends(748,-1))
+assert(not areFriends(10,20))
+
+'''
--- prog1/Ejercicios3v1/Ejercicio9.py
+++ prog1/Ejercicios3v1/Ejercicio9.py
@@ -14,7 +14,7 @@ def isPrime(number):
         if divisor==2:
             result=True
         else:
-            result=False            
+            result=False
     
     return result
 
@@ -26,7 +26,7 @@ def numberDivisors(number):
             if number%i==0:
                 divisors.append(i)
     else:
-        divisors=-1       
+        divisors=-1
     return divisors
 
 
@@ -40,14 +40,86 @@ assert(numberDivisors(582)==[1,2,3,6,97,194,291,582])
 
 
 def numberDivisorPrime(divisors):
-    count=0
+    primeDivisors=[]
     for i in divisors:
-        if isPrime(i)==True:
-            count+=1
+        if isPrime(i):
+            primeDivisors.append(i)
     
-    return count
+    return len(primeDivisors)
 
 
 assert(numberDivisorPrime([1,3,5,15])==2)
 
 
+'''
+Solución clase:
+
+def isPrime(number):
+    result = None 
+    if number>0:
+        prime = True
+        i = 2
+        while i<number//2+1 and prime:
+            if number%i==0:
+                prime = False
+            i+=1
+    
+    return prime
+    
+
+def numberDivisorPrime(number):
+    divisors = []
+    primeDivisors = []
+    size = -1
+    
+    if number > 0:
+        for i in range (1,(number//2)+1):
+            if number%i==0:
+                divisors.append(i)
+    
+        for div in divisors:
+            if divisors(div):
+                primeDivisors.append(div)
+    
+    return len(primeDivisors)
+
+
+
+Solución 2: más óptima
+
+def isPrime(number):
+    result = None 
+    if number>0:
+        prime = True
+        i = 2
+        while i<number//2+1 and prime:
+            if number%i==0:
+                prime = False
+            i+=1
+    
+    return prime
+    
+
+def numberDivisorPrime(number):
+    divisors = []
+    primeDivisors = []
+    size = -1
+    
+    if number > 0:
+        for i in range (1,number):
+            if number%i==0 and isPrime(number):
+                primeDivisors.append(i)
+        size = len(primeDivisors)
+    
+    return len(primeDivisors)
+
+assert(numberDivisorPrime(5)==1)
+assert(numberDivisorPrime(2)==1)
+assert(numberDivisorPrime(8)==2)
+assert(numberDivisorPrime(63)==3)
+assert(numberDivisorPrime(1)==0)
+assert(numberDivisorPrime(63)!=2)
+assert(numberDivisorPrime(0)==-1)
+assert(numberDivisorPrime(-2)==-1)
+
+'''
