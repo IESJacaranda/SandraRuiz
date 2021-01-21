bf27a603243b684b44589518f3d2543b5c2b0496
sandra <sandra.ruizjimenez86@gmail.com>
2021-01-21 08:39:42 +0100

    Creado

---
+++ java/src/string/primerEjercicio.java
@@ -0,0 +1,15 @@
+package string;
+
+import java.util.Scanner;
+
+public class primerEjercicio {
+
+	static Scanner teclado = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println();
+	}
+
+}
