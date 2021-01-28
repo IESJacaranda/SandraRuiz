f3dc8828d08851568114091bdcc12aa741d01615
sandra <sandra.ruizjimenez86@gmail.com>
2021-01-28 09:59:09 +0100

    Creado ejercicio

---
+++ java/src/string/Ejercicio8.java
@@ -0,0 +1,19 @@
+package string;
+
+public class Ejercicio8 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println();
+		
+	}
+	
+	public static String contarVocales(String frase){
+		
+		return vocales
+		
+		
+	}
+	
+	}
+}
--- java/src/string/primerEjercicio.java
+++ java/src/string/primerEjercicio.java
@@ -43,7 +43,7 @@ public class primerEjercicio {
 		
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String2[] args) {
 		//TODO Auto-generated method stub
 	
 			int num = charactersInString("Inmaculada Oalias", 'a');
