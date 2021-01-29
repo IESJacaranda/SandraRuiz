4d9836ab68d28a46231363ca986fd8229d51d0c6
sandra <sandra.ruizjimenez86@gmail.com>
2021-01-29 19:14:34 +0100

    Añadido ejercicio 10

--- java/src/string/Ejercicio10.java
+++ java/src/string/Ejercicio10.java
@@ -2,4 +2,32 @@ package string;
 
 public class Ejercicio10 {
 
+	public static void main(String[] args) {
+		
+		System.out.println(contarPalabras("no me gustan los lichis"));
+		System.out.println(contarPalabras("arrrrgggg no me gustan los lichis"));
+		System.out.println(contarPalabras("   arrrrgggg         no me   gustan los      lichis "));
+		System.out.println(contarPalabras("  no me gustan los lichis"));
+		System.out.println(contarPalabras("arrrrgggg no me gustan los lichis   "));
+		System.out.println(contarPalabras("   arrrrgggg         no me   gustan los      lichis "));
+		
+	}
+	
+	
+	//Este método recibe un String y devuelve el número de palabras que contiene
+	public static int contarPalabras(String cadena) {
+		int contadorPalabras=0;
+		cadena=" " + cadena;
+		
+		for(int i = 0;i<cadena.length()-1;i++) {
+			if (Character.isSpaceChar(cadena.charAt(i)) && Character.isLetter(cadena.charAt(i+1))) {
+				
+				contadorPalabras=contadorPalabras+1;
+			}
+		}
+
+		return contadorPalabras;
+	
+	}
+		
 }
