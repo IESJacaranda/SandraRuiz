76ee8fe4aab6c6059b5ed3ecccbc3061833efee7
sandra <sandra.ruizjimenez86@gmail.com>
2021-02-18 21:51:40 +0100

    Añadido pedir puntos

--- java/src/relacion7/ej1/Main.java
+++ java/src/relacion7/ej1/Main.java
@@ -1,21 +1,40 @@
 package relacion7.ej1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		
+			
+		/*
 		Punto puntito1 = new Punto(5.0, 3.0);
 		Punto puntito2 = new Punto(4.0, 2.0);
 		Punto puntito3 = new Punto(5.0, 3.0);
 		Punto puntito4 = new Punto(4.0, 2.0);
 		
-		//System.out.println(puntito1.equals(puntito2));
+		System.out.println(puntito1.equals(puntito2));
 
 		Linea linea1 = new Linea(puntito1,puntito2);
 		Linea linea2 = new Linea(puntito3,puntito4);
 		
-		System.out.println(linea1.equals(linea2));
+		System.out.println(linea1.equals(linea2));*/
 		
+	}
+	
+	public static void pedirDatos(Punto punto) {
+			
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Introduzca la coordenada x: ");
+		punto.setX(Double.parseDouble(teclado.nextLine()));
+		
+		punto.setX(Double.parseDouble(teclado.nextLine()));
+		
+		System.out.println("Introduzca la coordenada y: ");
+		punto.setY(Double.parseDouble(teclado.nextLine()));
+
 	}
 
 }
