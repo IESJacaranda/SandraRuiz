146c8a2b6ba565fae60257c1b719bd14e7417a1d
sandra <sandra.ruizjimenez86@gmail.com>
2021-02-25 10:00:44 +0100

    Clase 25/02

---
+++ java/src/relacion7/ej3/Jarra.java
@@ -0,0 +1,63 @@
+package relacion7.ej3;
+
+public class Jarra {
+
+	private double capacidad;
+	private double cantidadAgua;
+	private double totalAguaConsumida;
+	
+	public Jarra() {}
+	
+	public Jarra(double capacidad, double cantidadAgua) {
+		this.capacidad = capacidad;
+		this.cantidadAgua = 0.0;
+	}
+
+	public void llenarJarra() throws Exception {
+		if(this.cantidadAgua<this.capacidad
+				|| this.capacidad==0) {
+			this.cantidadAgua = this.capacidad;
+		}throw new Exception("La jarra no se puede llenar");
+	}
+	
+	public void vaciarJarra() throws Exception {
+		if(this.cantidadAgua==this.capacidad || this.capacidad>0) {
+			this.totalAguaConsumida=this.cantidadAgua;
+			this.cantidadAgua = 0.0;
+		}throw new Exception("La jarra no se puede vaciar");
+		
+	}
+	
+	public void volcarJarra () {
+		
+	}
+	
+	
+	public double getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(double capacidad) {
+		this.capacidad = capacidad;
+	}
+
+	public double getCantidadAgua() {
+		return cantidadAgua;
+	}
+
+	public void setCantidadAgua(double cantidadAgua) {
+		this.cantidadAgua = cantidadAgua;
+	}
+
+	public double getTotalAguaConsumida() {
+		return totalAguaConsumida;
+	}
+
+	public void setTotalAguaConsumida(double totalAguaConsumida) {
+		this.totalAguaConsumida = totalAguaConsumida;
+	}
+
+
+
+	
+}
---
+++ java/src/relacion7/ej3/Principal.java
@@ -0,0 +1,29 @@
+package relacion7.ej3;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		System.out.println("Introduce una opción A/B");
+		
+		String opcion = new Scanner(System.in).nextLine();
+		if(opcion=="A") {
+			System.out.println("Esta no va a comparar");
+		}else if(opcion.equalsIgnoreCase("A")){ //el IgnoreCase ignora mayúsculas y minúsculas
+			System.out.println("Esta sí que compara");
+		}
+		*/
+		
+		Jarra jarraA = new Jarra();
+		Jarra jarraB = new Jarra();
+		
+		System.out.println("Introduce la capacidad de la jarra A");
+		
+		
+	}
+
+}
